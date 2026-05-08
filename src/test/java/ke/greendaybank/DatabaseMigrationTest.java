e1295dc54c02b8b58bda08800465e7f4b9574cc6
Imran shiundu <143967151+imranshiundu@users.noreply.github.com>
2026-05-08 11:42:58 +0300

    Add database migration integration test

---
+++ src/test/java/ke/greendaybank/DatabaseMigrationTest.java
@@ -0,0 +1,11 @@
+package ke.greendaybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMigrationTest {
+    @Test
+    void contextLoads() {
+    }
+}
