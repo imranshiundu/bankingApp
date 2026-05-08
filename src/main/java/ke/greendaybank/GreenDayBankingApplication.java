e375bd84d845df103eec287586df3d8238b91e97
Imran shiundu <143967151+imranshiundu@users.noreply.github.com>
2026-05-08 11:36:32 +0300

    Add Spring Boot application entrypoint

---
+++ src/main/java/ke/greendaybank/GreenDayBankingApplication.java
@@ -0,0 +1,11 @@
+package ke.greendaybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenDayBankingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenDayBankingApplication.class, args);
+    }
+}
